5dab4ae6b9e5af3e3df5b5a09848d1fba916f6a6
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2020-08-15 21:02:13 +0800

    13

--- RPG/Assets/Script/Learn.cs
+++ RPG/Assets/Script/Learn.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class Learn : MonoBehaviour
 {
     public int i;
--- RPG/Assets/Script/meauman.cs
+++ RPG/Assets/Script/meauman.cs
@@ -1,10 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class meauman : MonoBehaviour
 {
+    [Header("載入畫面")]
+    public GameObject Loading;
+    [Header("文字")]
+    public Text Text;
+    [Header("進度條")]
+    public Image Ima;
+    [Header("載入場景")]
+    public string namescenc = "遊戲關卡";
 
     public void Quit()
     {
         Application.Quit();
+    }
+    public void StartGame()
+    {
+        StartCoroutine(Loaading());
+    }
+    private IEnumerator Loaading()
+    {
+        Loading.SetActive(true);
+       AsyncOperation ao= SceneManager.LoadSceneAsync(namescenc);
+        ao.allowSceneActivation = false;
+        while (!ao.isDone)
+        {
+            Text.text = ao.progress * 100 + "%";
+            Ima.fillAmount = ao.progress;
+            yield return null;
+        }
+
     }
 }
