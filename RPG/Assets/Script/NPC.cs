3c72efdd277d7ff9084e27e5f6a7ade6461d0009
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2020-08-29 15:58:05 +0800

    對話

---
+++ RPG/Assets/Script/NPC.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCDate Date;
+    [Header("名稱")]
+    public Text TextName;
+    [Header("對話區塊")]
+    public GameObject Dialongs;
+    [Header("內容")]
+    public Text TextC;
+
+
+    /// <summary>
+    /// 對話系統
+    /// </summary>
+    public void Dialong()
+    {
+        Dialongs.SetActive(true);
+        TextName.text=name;
+        TextC.text = Date.dialongs[0];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "月") Dialong();
+    }
+}
---
+++ RPG/Assets/Script/NPCDate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public enum NPCState
+{
+    NMission,Missing,Finish
+}
+//ScriptableObject 腳本化物件:可儲存於專案的資料
+[CreateAssetMenu(fileName ="NPC 資料",menuName ="R/ NPC 資料")]
+
+public class NPCDate : ScriptableObject
+{
+    [Header("NPC 狀態")]
+    public NPCState _NPCStste = NPCState.NMission;
+    [Header("任務數量")]
+    public int Count;
+    [Header("對話")]
+    public string[] dialongs = new string[3];
+}
